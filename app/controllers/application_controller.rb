27c8830088d8ef91c05cdbbbf8581e38212c60c8
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2014-06-22 20:29:55 +0200

    fixed variable name -- clash with active admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :set_circles
+  before_filter :menu_circles
 
-  def set_circles
-    @circles = Circle.all
+  def menu_circles
+    @menu_circles = Circle.all
   end
 end
