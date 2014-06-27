a474cba82f45edfc08cef33f835da471b3dc1e7b
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2014-06-27 19:23:35 +0200

    editable circle code

--- app/admin/circle.rb
+++ app/admin/circle.rb
@@ -1,20 +1,23 @@
 ActiveAdmin.register Circle do
-  permit_params :name
+  permit_params :name, :code
 
   index do
     selectable_column
     id_column
     column :name
+    column :code
     column :created_at
     actions
   end
 
   filter :name
+  filter :code
   filter :created_at
 
   form do |f|
     f.inputs "Circle Details" do
       f.input :name
+      f.input :code
     end
     f.actions
   end
