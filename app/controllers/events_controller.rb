8763efe096a505865fdae1226ad2637642d7494d
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2014-08-09 23:32:15 +0200

    find or initialize rsvp for event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
-    @rsvp = Rsvp.find_by(event: @event, user: current_user)
+    @rsvp = Rsvp.find_or_initialize_by(event: @event, user: current_user)
     @going_users = User.where(id: @event.rsvps.going.pluck(:user_id)).includes(:profile)
     @waiting_users = User.where(id: @event.rsvps.waiting.pluck(:user_id)).includes(:profile)
   end
