42fa680c203d153464ffefd2d39d2cee90dbe0d7
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2014-09-09 09:33:54 +0200

    route translator

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,22 @@ Rails.application.routes.draw do
   devise_for :admins, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  root 'dashboard#index'
 
-  resources :circles, only: [:show], path: 'latajace-kregi'
+  localized do
+    root 'dashboard#index'
 
-  resources :events, only: [:show], path: 'wydarzenia' do
-    resources :rsvps, only: [:create, :destroy]
+    resource :profile, only: [:edit, :update]
+
+    resources :circles, only: [:show]
+
+    resources :events, only: [:show] do
+      resources :rsvps, only: [:create, :destroy]
+    end
+
+    resources :heroines, only: [:index, :show]
+    resources :organizers, only: [:index, :show]
+    resources :people, only: [:index, :show]
   end
-
-  scope(path_names: { edit: 'edycja' }) do
-    resource :profile, path: 'profil'
-  end
-
-  resources :people, only: [:index, :show], path: 'kregowiczki'
-  resources :heroines, only: [:index, :show], path: 'bohaterki'
-  resources :organizers, only: [:index, :show], path: 'organizatorki'
 
   resources :tags
 end
