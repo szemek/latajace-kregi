fc0bc6d8af73c60fb868364f9b608c8965244920
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2014-09-20 09:17:20 +0200

    set locale to :en when hitting admin area

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_locale
+
+  def set_locale
+    I18n.locale = :en if admin_area?
+  end
+
+  def admin_area?
+    request.path.include?('/admin')
+  end
 
   def circles
     @_circles ||= Circle.order(:name)
