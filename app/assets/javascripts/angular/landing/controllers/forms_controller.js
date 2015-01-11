fbf583899447fe4609b9c2241f95bce0c4752b1b
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-01-11 13:46:51 +0100

    hide intro when displaying overlay

--- app/assets/javascripts/angular/landing/controllers/forms_controller.js
+++ app/assets/javascripts/angular/landing/controllers/forms_controller.js
@@ -1,5 +1,7 @@
 app.controller('FormsController', ['$scope', function($scope){
   $scope.showModal = function() {
+    $('.intro').hide();
+
     $.post('/api/users/check', $('#check form').serialize(), function(data){
       $scope[data.action] = true;
       $scope.$apply();
@@ -7,6 +9,8 @@ app.controller('FormsController', ['$scope', function($scope){
   };
 
   $scope.closeModal = function() {
+    $('.intro').show();
+
     $scope.signin = false;
     $scope.signup = false;
   }
