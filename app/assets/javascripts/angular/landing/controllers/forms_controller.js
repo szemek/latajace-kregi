2b7f4a328f4ebcf5deab3956a85e433bba85a08b
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-01-14 14:50:44 +0100

    form steps

--- app/assets/javascripts/angular/landing/controllers/forms_controller.js
+++ app/assets/javascripts/angular/landing/controllers/forms_controller.js
@@ -1,8 +1,32 @@
 app.controller('FormsController', ['$scope', function($scope){
+  $scope.showEmail = true;
+  $scope.showNext = true;
+
   $scope.showModal = function() {
     $('.intro').hide();
 
     $scope.modal = true;
+  };
+
+  $scope.go = {
+    signin: function() {
+      $scope.showNext = false;
+      $scope.showSignin = true;
+      $scope.showPassword = true;
+    },
+    signup: function() {
+      $scope.showNext = false;
+      $scope.showSignup = true;
+      $scope.showPassword = true;
+      $scope.showName = true;
+    }
+  };
+
+  $scope.check = function() {
+    $.post('/api/users/check', $('form').serialize(), function(data){
+      $scope.go[data.action]();
+      $scope.$apply();
+    });
   };
 
   $scope.closeModal = function() {
