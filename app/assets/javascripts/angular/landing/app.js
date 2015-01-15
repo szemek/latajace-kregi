b37561d1eab7125b70a7345c652859f87c9e5fa3
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-01-15 23:36:37 +0100

    localized error when email has been taken

--- app/assets/javascripts/angular/landing/app.js
+++ app/assets/javascripts/angular/landing/app.js
@@ -2,11 +2,13 @@ var app = angular.module('landing', ['pascalprecht.translate']);
 
 app.config(['$translateProvider', function ($translateProvider) {
   $translateProvider.translations('en', {
+    email: 'Email',
     password: 'Password',
     password_confirmation: 'Password'
   });
 
   $translateProvider.translations('pl', {
+    email: 'Email',
     password: 'Hasło',
     password_confirmation: 'Hasło'
   });
