b1870921fc430823dd39a0ea9dcbb9f74cc65f6a
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-01-15 20:25:29 +0100

    spinner
    http://samherbert.net/svg-loaders/

--- app/assets/javascripts/angular/landing/controllers/forms_controller.js
+++ app/assets/javascripts/angular/landing/controllers/forms_controller.js
@@ -33,20 +33,28 @@ app.controller('FormsController', ['$scope', function($scope) {
   };
 
   $scope.check = function() {
+    $scope.spinner = true;
     $.post('/api/users/check', $('form').serialize(), function(data) {
       $scope.go[data.action]();
+      $scope.spinner = false;
       $scope.$apply();
     });
   };
 
   $scope.signin = function() {
+    $scope.spinner = true;
     $.post('/api/users/sign_in', $('form').serialize(), function(data) {
+      $scope.spinner = false;
+      $scope.$apply();
       window.location.replace('/dashboard');
     }, 'json');
   }
 
   $scope.signup = function() {
+    $scope.spinner = true;
     $.post('/api/users', $('form').serialize(), function(data) {
+      $scope.spinner = false;
+      $scope.$apply();
       window.location.replace('/dashboard');
     }, 'json');
   }
