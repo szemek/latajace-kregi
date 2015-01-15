241b3cda8dd4314beefd232a8621ba060124c0cc
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-01-15 22:19:16 +0100

    underscore-rails gem added

--- app/assets/javascripts/angular/landing.js
+++ app/assets/javascripts/angular/landing.js
@@ -2,6 +2,7 @@
 //= require jquery_ujs
 //= require angular
 //= require angular-translate
+//= require underscore
 //= require ./landing/app
 //= require_tree ./landing/controllers
 //= require_tree ./landing/directives
