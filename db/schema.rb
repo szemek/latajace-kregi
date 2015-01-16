6047905bb4f9e6f9a7f1584155db60fda849951d
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-01-16 12:40:39 +0100

    feedbacks table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150116095727) do
+ActiveRecord::Schema.define(version: 20150116112640) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -65,6 +65,13 @@ ActiveRecord::Schema.define(version: 20150116095727) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "quantity",    default: 20
+  end
+
+  create_table "feedbacks", force: true do |t|
+    t.integer  "user_id"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "friendly_id_slugs", force: true do |t|
