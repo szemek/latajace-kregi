4e49ca04cc1335ce773cd15b910460bd436e8bd1
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-01-19 07:33:22 +0100

    devise: timeout session in 7 days

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
+
+  def remember_me
+    true
+  end
 
   has_one :profile, dependent: :destroy
   has_many :rsvps
