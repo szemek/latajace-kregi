43b167dacb04183b58361df59f6b20c3973da0b0
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-01-20 22:57:57 +0100

    checking email by downcase version
    email is downcased by default in devise

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,11 +1,17 @@
 class Api::UsersController < Api::BaseController
   def check
-    user = User.find_by_email(params[:user][:email])
+    user = User.find_by_email(email)
 
     if user.present?
       render json: {action: 'signin'}
     else
       render json: {action: 'signup'}
     end
+  end
+
+  private
+
+  def email
+    params[:user][:email].downcase
   end
 end
