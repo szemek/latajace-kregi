fa6a39ad89cad47b96a1ebfb00c1a8a0065ce340
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-01-23 08:48:44 +0100

    updated filled scope to skip profiles with empty string

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,7 +6,7 @@ class Profile < ActiveRecord::Base
 
   acts_as_ordered_taggable_on :skills, :interests
 
-  scope :filled, -> { where.not(fullname: nil) }
+  scope :filled, -> { where('fullname IS NOT NULL AND LENGTH(fullname) > 0') }
 
   delegate :code, to: :circle, allow_nil: true
   delegate :events, to: :user, allow_nil: true
