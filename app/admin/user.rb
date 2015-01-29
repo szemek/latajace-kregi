52c38030d06ab1d2c58f49749c0e77449c97a9bf
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-01-29 20:18:05 +0100

    for user allow to change email & admin flag

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :admin
 
   actions :all, except: [:destroy]
 
@@ -22,8 +22,7 @@ ActiveAdmin.register User do
   form do |f|
     f.inputs "User Details" do
       f.input :email
-      f.input :password
-      f.input :password_confirmation
+      f.input :admin
     end
     f.actions
   end
