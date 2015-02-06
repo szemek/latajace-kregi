4022f042a34782009df01eb1a56b8d9f6bf0dcd7
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-02-06 20:24:14 +0100

    singular form when looking for skill or interest

--- app/searches/profile_search.rb
+++ app/searches/profile_search.rb
@@ -1,17 +1,17 @@
 class ProfileSearch < Searchlight::Search
   search_on proc { Profile.filled.joins(:user) }
 
-  searches :circle_id, :skills, :interests
+  searches :circle_id, :skill, :interest
 
   def search_circle_id
     search.where(circle_id: circle_id)
   end
 
-  def search_skills
-    search.tagged_with(skills, on: :skills, any: true)
+  def search_skill
+    search.tagged_with(skill, on: :skills, any: true)
   end
 
-  def search_interests
-    search.tagged_with(interests, on: :interests, any: true)
+  def search_interest
+    search.tagged_with(interest, on: :interests, any: true)
   end
 end
