8da9973db9f93ca6860b48b12c710277407393d4
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-02-08 09:01:04 +0100

    added before_action :authenticate_user! to prevent access when user is not signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :set_locale
-  before_filter :authorize_admin, if: :admin_panel?
 
-  def set_locale
-    I18n.locale = :en if admin_panel?
-  end
-
-  def current_locale
-    I18n.locale || :en
-  end
-
-  helper_method :current_locale
+  before_action :authenticate_user!
+  before_action :authorize_admin, if: :admin_panel?
 
   def admin_panel?
     request.path.start_with?('/admin')
--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -7,11 +7,24 @@ describe PeopleController do
   context 'GET' do
     let!(:profile) { create(:profile) }
 
-    it 'returns all profiles' do
-      get :index
+    context 'user is not logged in' do
+      it 'redirects' do
+        get :index
 
-      expect(response.status).to eq(200)
-      expect(response.body).to include(profile.fullname)
+        expect(response.status).to eq(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'user is logged in' do
+      it 'returns all profiles' do
+        sign_in(profile.user)
+
+        get :index
+
+        expect(response.status).to eq(200)
+        expect(response.body).to include(profile.fullname)
+      end
     end
   end
 end
