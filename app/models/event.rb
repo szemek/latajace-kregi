8158a49a1af25fb0b4911f624efc7be55605f52e
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-02-12 10:10:30 +0100

    event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ActiveRecord::Base
   belongs_to :circle
   has_many :rsvps, dependent: :delete_all
   has_many :users, through: :rsvps
+
+  validates :name, presence: true
+  validates :starting_at, presence: true
+  validates :circle, presence: true
 end
