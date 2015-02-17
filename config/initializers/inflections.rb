50dcd67495670150cdb30f9ac593dc793e01090f
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-02-17 10:14:00 +0100

    inflections for FAQ

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -14,3 +14,8 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
 # end
+
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.plural /^(faq)$/i, '\1'.upcase
+  inflect.acronym 'FAQ'
+end
