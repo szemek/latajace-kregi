fa31b94342f882fadec1bdfa52e46388a940bbbc
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-03-10 12:01:15 +0100

    after sign up redirect to profile edit page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'members#index'
 
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'registrations'
+  }
   ActiveAdmin.routes(self)
 
   post 'users/check'
