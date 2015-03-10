8b6060cff82a82fdc455c6793a9953ecb2cb3720
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-03-10 23:46:13 +0100

    customized show page for user in activeadmin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -22,6 +22,33 @@ ActiveAdmin.register User do
     actions
   end
 
+  show do |user|
+    attributes_table do
+      row :id
+      row :email
+      row :admin
+      row :profile do
+        user.profile
+      end
+    end
+
+    panel 'Authentication Details' do
+      attributes_table_for user do
+        row :encrypted_password
+        row :reset_password_token
+        row :reset_password_sent_at
+        row :remember_created_at
+        row :sign_in_count
+        row :current_sign_in_at
+        row :last_sign_in_at
+        row :current_sign_in_ip
+        row :last_sign_in_ip
+        row :created_at
+        row :updated_at
+      end
+    end
+  end
+
   filter :email
   filter :current_sign_in_at
   filter :sign_in_count
