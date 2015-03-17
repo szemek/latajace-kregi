1786d81f98d5c9f5babb5a8146b1518404d14a36
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-03-18 00:18:33 +0100

    after sign in redirect to session['user_return_to'] location

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'members#index'
 
   devise_for :users, :controllers => {
-    :registrations => 'registrations'
+    registrations: 'registrations',
+    sessions: 'sessions'
   }
   ActiveAdmin.routes(self)
 
