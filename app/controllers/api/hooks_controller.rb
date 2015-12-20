863709a0359f0d4747f43bf693e808bf4ef46812
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-12-20 12:01:46 +0100

    don't create email in hook

--- app/controllers/api/hooks_controller.rb
+++ app/controllers/api/hooks_controller.rb
@@ -1,17 +1,5 @@
 class Api::HooksController < Api::BaseController
   def email
-    Email.create(email_params)
-
-    render json: {}, status: :created
-  end
-
-  def email_params
-    {
-      from_name: params['FromName'],
-      from: params['From'],
-      subject: params['Subject'],
-      html_body: params['HtmlBody'],
-      text_body: params['TextBody']
-    }
+    render json: {}, status: :ok
   end
 end
